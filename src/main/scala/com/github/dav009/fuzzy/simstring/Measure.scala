e994252406f74a280ea19a3395095fa00af93889
David Przybilla <dav.alejandro@gmail.com>
2015-03-20 19:25:58 +0000

    Adding Jaccard/Dice

--- src/main/scala/com/github/dav009/fuzzy/simstring/Measure.scala
+++ src/main/scala/com/github/dav009/fuzzy/simstring/Measure.scala
@@ -22,3 +22,35 @@ object Cosine extends Measure{
 
 
 }
+
+object Jaccard extends Measure{
+
+
+  override def t(threshold:Double, sizeOfFeaturesA:Int, sizeOfFeaturesB:Int):Int={
+    Math.ceil( (threshold*(sizeOfFeaturesA + sizeOfFeaturesB)) / (1 + threshold)).toInt
+  }
+
+  override def max(threshold:Double, sizeOfFeaturesA:Int):Int={
+    Math.floor(sizeOfFeaturesA/threshold).toInt
+  }
+
+  override def min(threshold:Double, sizeOfFeaturesA:Int):Int={
+    Math.ceil(threshold * sizeOfFeaturesA).toInt
+  }
+}
+
+object Dice extends Measure{
+
+
+  override def t(threshold:Double, sizeOfFeaturesA:Int, sizeOfFeaturesB:Int):Int={
+    Math.ceil( 0.5 *  threshold * (sizeOfFeaturesA + sizeOfFeaturesB) ).toInt
+  }
+
+  override def max(threshold:Double, sizeOfFeaturesA:Int):Int={
+    Math.floor( ((2-threshold)/threshold) * sizeOfFeaturesA ).toInt
+  }
+
+  override def min(threshold:Double, sizeOfFeaturesA:Int):Int={
+    Math.ceil( (threshold/(2-threshold)) * sizeOfFeaturesA ).toInt
+  }
+}
